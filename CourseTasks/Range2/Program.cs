d34d02c584e3e09d92c37bfaf8de1409b1afc5a3
Сергей Игнатов <IgnatSergeu@gmail.com>
2019-07-16 21:58:07 +0700

    Внес исправления в задачу Range*

--- CourseTasks/Range2/Program.cs
+++ CourseTasks/Range2/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Academits
 {
@@ -12,20 +8,16 @@ namespace Academits
         {
             double from1 = -8;
             double to1 = 15;
-            double from2 = 17;
-            double to2 = 27;
+            double from2 = -16;
+            double to2 = -9;
             double number = 20;
 
-            Range range1 = new Range();
-            Range range2 = new Range();
-            range1.From = from1;
-            range1.To = to1;
-            range2.From = from2;
-            range2.To = to2;
+            Range range1 = new Range(from1, to1);
+            Range range2 = new Range(from2, to2);
 
-            Range rangeCross = Range.GetCrossing(range1, range2);
-            Range[] rangesUnion = Range.GetUnion(range1, range2);
-            Range[] rangesResidual = Range.GetResidual(range1, range2);
+            Range rangeCross = range1.GetCrossing(range2);
+            Range[] rangesUnion = range1.GetUnion(range2);
+            Range[] rangesResidual = range1.GetResidual(range2);
 
             Console.WriteLine("Длина интервала [{0};{1}] : ", from1, to1);
             Console.WriteLine(range1.GetLength());
@@ -53,31 +45,25 @@ namespace Academits
             Console.WriteLine(new string('-', 50));
 
             Console.WriteLine("Объединение интервала [{0};{1}] и интервала [{2};{3}]:", from1, to1, from2, to2);
-            if (rangesUnion[1] == null)
+            foreach (Range e in rangesUnion)
             {
-                rangesUnion[0].PrintRange();
-            }
-            else
-            {
-                rangesUnion[0].PrintRange();
-                rangesUnion[1].PrintRange();
+                e.PrintRange();
             }
             Console.WriteLine(new string('-', 50));
 
             Console.WriteLine("Разность интервала [{0};{1}] и интервала [{2};{3}]:", from1, to1, from2, to2);
-            if (rangesResidual[0] == null)
+            if (rangesResidual == null)
             {
                 Console.WriteLine("пустое множество");
             }
-            else if (rangesResidual[1] == null)
-            {
-                rangesResidual[0].PrintRange();
-            }
             else
             {
-                rangesResidual[0].PrintRange();
-                rangesResidual[1].PrintRange();
+                foreach (Range e in rangesResidual)
+                {
+                    e.PrintRange();
+                }
             }
+            Console.WriteLine(new string('-', 50));
         }
     }
 }
--- CourseTasks/Range2/Range.cs
+++ CourseTasks/Range2/Range.cs
@@ -1,23 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Academits
 {
     class Range
     {
-        public double From
-        {
-            get;
-            set;
-        }
+        public double From { get; set; }
+        public double To { get; set; }
 
-        public double To
+        public Range(double from, double to)
         {
-            get;
-            set;
+            From = from;
+            To = to;
         }
 
         public void PrintRange()
@@ -32,150 +25,132 @@ namespace Academits
 
         public bool IsInside(double number)
         {
-            if (number > From && number < To)
-            {
-                return true;
-            }
-            return false;
+            return number > From && number < To;
         }
 
-        public static int GetTypeCrossin(Range x, Range y)
+        public enum CrossingType
         {
-            int typeCrossing = 0;
-            double a = x.From;
-            double b = x.To;
-            double c = y.From;
-            double d = y.To;
-
-            if (c >= a && c <= b && b <= d)
-            {
-                typeCrossing = 1;
-            }
-            else if (a >= c && a <= d && d <= b)
-            {
-                typeCrossing = 2;
-            }
-            else if (c >= a && d <= b)
-            {
-                typeCrossing = 3;
-            }
-            else if (a >= c && b <= d)
-            {
-                typeCrossing = 4;
-            }
-            return typeCrossing;
+            CrossingThisRangeRight = 0,
+            CrossingThisRangeLeft = 1,
+            ContainingRange = 2,
+            ContainingThisRange = 3,
+            NotCrossing = 4
         }
 
-        public static Range GetCrossing(Range x, Range y)
+        public CrossingType GetCrossingType(Range range)
         {
-            double fromCrossing;
-            double toCrossing;
-            double a = x.From;
-            double b = x.To;
-            double c = y.From;
-            double d = y.To;
+            double startThisRange = From;
+            double endThisRange = To;
+            double startRange = range.From;
+            double endRange = range.To;
 
-            int typeCrossing = GetTypeCrossin(x, y);
-            if (typeCrossing == 1)
+            if (startRange >= startThisRange && startRange <= endThisRange && endThisRange <= endRange)
             {
-                fromCrossing = c;
-                toCrossing = b;
+                return CrossingType.CrossingThisRangeRight;
             }
-            else if (typeCrossing == 2)
+            else if (startThisRange >= startRange && startThisRange <= endRange && endRange <= endThisRange)
             {
-                fromCrossing = a;
-                toCrossing = d;
+                return CrossingType.CrossingThisRangeLeft;
             }
-            else if (typeCrossing == 3)
+            else if (startRange >= startThisRange && endRange <= endThisRange)
             {
-                fromCrossing = c;
-                toCrossing = d;
+                return CrossingType.ContainingRange;
             }
-            else if (typeCrossing == 4)
+            else if (startThisRange >= startRange && endThisRange <= endRange)
             {
-                fromCrossing = a;
-                toCrossing = b;
+                return CrossingType.ContainingThisRange;
+            }
+            return CrossingType.NotCrossing;
+        }
+
+        public Range GetCrossing(Range range)
+        {
+            double startThisRange = From;
+            double endThisRange = To;
+            double startRange = range.From;
+            double endRange = range.To;
+
+            CrossingType typeCrossing = GetCrossingType(range);
+            if ((int)typeCrossing == 0)
+            {
+                return new Range(startRange, endThisRange);
+            }
+            else if ((int)typeCrossing == 1)
+            {
+                return new Range(startThisRange, endRange);
+            }
+            else if ((int)typeCrossing == 2)
+            {
+                return new Range(startRange, endRange);
+            }
+            else if ((int)typeCrossing == 3)
+            {
+                return new Range(startThisRange, endThisRange);
             }
             else
             {
                 return null;
             }
-            Range rangeCross = new Range();
-            rangeCross.From = fromCrossing;
-            rangeCross.To = toCrossing;
-            return rangeCross;
         }
 
-        public static Range[] GetUnion(Range x, Range y)
+        public Range[] GetUnion(Range range)
         {
-            Range[] arrayRangesUnion = new Range[2];
-            arrayRangesUnion[0] = new Range();
-            arrayRangesUnion[1] = new Range();
-            if (GetCrossing(x, y) == null)
+            double startThisRange = From;
+            double endThisRange = To;
+            double startRange = range.From;
+            double endRange = range.To;
+
+            if (endThisRange < startRange || startThisRange > endRange)
             {
-                arrayRangesUnion[0] = x;
-                arrayRangesUnion[1] = y;
+                Range[] arrayRangesUnion = new Range[2];
+                arrayRangesUnion[0] = new Range(startThisRange, endThisRange);
+                arrayRangesUnion[1] = new Range(startRange, endRange);
+                return arrayRangesUnion;
             }
             else
             {
-                double minFrom = x.From;
-                double maxTo = x.To;
-                if (minFrom > y.From)
-                {
-                    minFrom = y.From;
-                }
-                if (maxTo < y.To)
-                {
-                    maxTo = y.To;
-                }
-                arrayRangesUnion[0].From = minFrom;
-                arrayRangesUnion[0].To = maxTo;
-                arrayRangesUnion[1] = null;
+                Range[] arrayRangesUnion = new Range[1];
+                arrayRangesUnion[0] = new Range(Math.Min(startThisRange, startRange), Math.Max(endThisRange, endRange));
+                return arrayRangesUnion;
             }
-            return arrayRangesUnion;
         }
 
-        public static Range[] GetResidual(Range x, Range y)
+        public Range[] GetResidual(Range range)
         {
-            double a = x.From;
-            double b = x.To;
-            double c = y.From;
-            double d = y.To;
-            Range[] arrayRangesResidual = new Range[2];
-            arrayRangesResidual[0] = new Range();
-            arrayRangesResidual[1] = new Range();
+            double startThisRange = From;
+            double endThisRange = To;
+            double startRange = range.From;
+            double endRange = range.To;
 
-            int typeCrossing = GetTypeCrossin(x, y);
-            if (typeCrossing == 0)
+            if (endThisRange < startRange || startThisRange > endRange)
             {
-                arrayRangesResidual[0] = x;
-                arrayRangesResidual[1] = null;
+                Range[] arrayRangesResidual = new Range[1];
+                arrayRangesResidual[0] = new Range(startThisRange, endThisRange);
+                return arrayRangesResidual;
             }
-            else if (typeCrossing == 1)
+            else if (startRange < startThisRange && endThisRange < endRange)
             {
-                arrayRangesResidual[0].From = a;
-                arrayRangesResidual[0].To = c;
-                arrayRangesResidual[1] = null;
+                return null;
             }
-            else if (typeCrossing == 2)
+            else if (startRange < startThisRange)
             {
-                arrayRangesResidual[0].From = a;
-                arrayRangesResidual[0].To = d;
-                arrayRangesResidual[1] = null;
+                Range[] arrayRangesResidual = new Range[1];
+                arrayRangesResidual[0] = new Range(endRange, endThisRange);
+                return arrayRangesResidual;
             }
-            else if (typeCrossing == 3)
+            else if (startThisRange < startRange && endThisRange > endRange)
             {
-                arrayRangesResidual[0].From = a;
-                arrayRangesResidual[0].To = c;
-                arrayRangesResidual[1].From = d;
-                arrayRangesResidual[1].To = b;
+                Range[] arrayRangesResidual = new Range[2];
+                arrayRangesResidual[0] = new Range(startThisRange, startRange);
+                arrayRangesResidual[1] = new Range(endRange, endThisRange);
+                return arrayRangesResidual;
             }
             else
             {
-                arrayRangesResidual[0] = null;
-                arrayRangesResidual[1] = null;
+                Range[] arrayRangesResidual = new Range[1];
+                arrayRangesResidual[0] = new Range(startThisRange, startRange);
+                return arrayRangesResidual;
             }
-            return arrayRangesResidual;
         }
     }
 }
