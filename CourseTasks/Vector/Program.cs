771ab25990ca35fa25ff885c4d32bc9f46e6f16f
Сергей Игнатов <IgnatSergeu@gmail.com>
2019-07-23 19:27:31 +0700

    Добавил задачу Vector

---
+++ CourseTasks/Vector/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Vector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Vector zerowVector = new Vector(6);
+                Console.WriteLine("zerowVector = " + zerowVector);
+
+                double[] array = { 3, 3.75, 2, 44, -5 };
+                Vector testVector = new Vector(array);
+                Vector copyTestVector = new Vector(testVector);
+                Console.WriteLine("testVector = " + testVector);
+                Console.WriteLine("copyTestVector = " + copyTestVector);
+
+                Vector partialTestVector = new Vector(15, array);
+                Console.WriteLine("partialTestVector = " + partialTestVector);
+                Console.WriteLine("Размерность partialTesVector = " + partialTestVector.GetSize());
+
+                Vector firstVector = new Vector(new double[] { 3, 3, 4, 4 });
+                Console.WriteLine("Сумма векторов {0} и {1} равна: {2}", firstVector, testVector, firstVector.GetSum(testVector));
+                Console.WriteLine("Разность векторов {0} и {1} равна: {2}", firstVector.GetSum(testVector), testVector, firstVector.GetSum(testVector).GetResidual(testVector));
+                Console.WriteLine("Результат умножения вектора {0} на скаляр {1}: {2}", firstVector, 3, firstVector.GetScalarMultiplication(3));
+
+                Console.Write("Вектор обратный вектору {0}:", firstVector);
+                firstVector.GetOpposite();
+                Console.WriteLine(firstVector);
+
+                Console.WriteLine("Длина вектора {0} : {1:f2}", firstVector, firstVector.GetLength());
+
+                Console.WriteLine("Компонента вектора {0} по индексу {1} : {2}", firstVector, 3, firstVector.GetComponent(3));
+                firstVector.SetComponent(3, 55.6);
+                Console.WriteLine("Компонента вектора {0} по индексу {1} : {2}", firstVector, 3, firstVector.GetComponent(3));
+
+                Vector secondVector = new Vector(new double[] { -3, -3, -4, -5, 0, 0 });
+                if (firstVector.Equals(secondVector))
+                {
+                    Console.WriteLine("Вектор {0} равен вектору {1}", firstVector, secondVector);
+                }
+                else
+                {
+                    Console.WriteLine("Вектор {0} не равен вектору {1}", firstVector, secondVector);
+                }
+                Console.WriteLine("hashCode вектора {0}: {1}", firstVector, firstVector.GetHashCode());
+                Console.WriteLine("hashCode вектора {0}: {1}", secondVector, secondVector.GetHashCode());
+
+
+                Console.WriteLine("Сумма векторов {0} и {1} равна: {2}", firstVector, secondVector, Vector.GetSum(firstVector, secondVector));
+                Console.WriteLine("Разность векторов {0} и {1} равна: {2}", Vector.GetSum(firstVector, secondVector), firstVector, Vector.GetResidual(Vector.GetSum(firstVector, secondVector), firstVector));
+                Console.WriteLine("Результат умножения вектора {0} на скаляр {1}: {2}", firstVector, 3, Vector.GetScalarMultiplication(firstVector, 3));
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
---
+++ CourseTasks/Vector/Vector.cs
@@ -0,0 +1,239 @@
+﻿using System;
+
+namespace Vector
+{
+    class Vector
+    {
+        public double[] Components { get; set; }
+
+        public Vector(int n)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentException("Размерность вектора должа быть больше 0");
+            }
+            Components = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                Components[i] = 0;
+            }
+        }
+
+        public Vector(Vector secondVector)
+        {
+            Components = new double[secondVector.Components.Length];
+            for (int i = 0; i < secondVector.Components.Length; i++)
+            {
+                Components[i] = secondVector.Components[i];
+            }
+        }
+
+        public Vector(double[] array)
+        {
+            Components = new double[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Components[i] = array[i];
+            }
+        }
+
+        public Vector(int n, double[] array)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentException("Размерность вектора должа быть больше 0");
+            }
+            Components = new double[n];
+            if (n > array.Length)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Components[i] = array[i];
+                }
+                for (int i = array.Length + 1; i < n; i++)
+                {
+                    Components[i] = 0;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Components[i] = array[i];
+                }
+            }
+        }
+
+        public int GetSize()
+        {
+            return Components.Length;
+        }
+
+        public Vector GetSum(Vector secondVector)
+        {
+            int maxDimension = GetSize();
+            if (maxDimension < secondVector.GetSize())
+            {
+                maxDimension = secondVector.GetSize();
+            }
+            Vector copySecondVector = new Vector(maxDimension, secondVector.Components);
+            Vector copyFirstVector = new Vector(maxDimension, Components);
+            Vector sumVector = new Vector(maxDimension);
+            for (int i = 0; i < maxDimension; i++)
+            {
+                sumVector.Components[i] = copyFirstVector.Components[i] + copySecondVector.Components[i];
+            }
+            return sumVector;
+        }
+
+        public Vector GetResidual(Vector secondVector)
+        {
+            int maxDimension = GetSize();
+            if (maxDimension < secondVector.GetSize())
+            {
+                maxDimension = secondVector.GetSize();
+            }
+            Vector copySecondVector = new Vector(maxDimension, secondVector.Components);
+            Vector copyFirstVector = new Vector(maxDimension, Components);
+            Vector sumVector = new Vector(maxDimension);
+            for (int i = 0; i < maxDimension; i++)
+            {
+                sumVector.Components[i] = copyFirstVector.Components[i] - copySecondVector.Components[i];
+            }
+            return sumVector;
+        }
+
+        public Vector GetScalarMultiplication(double scalar)
+        {
+            Vector resultVector = new Vector(GetSize());
+            for (int i = 0; i < GetSize(); i++)
+            {
+                resultVector.Components[i] = Components[i] * scalar;
+            }
+            return resultVector;
+        }
+
+        public void GetOpposite()
+        {
+            for (int i = 0; i < GetSize(); i++)
+            {
+                Components[i] = Components[i] * (-1);
+            }
+        }
+
+        public double GetLength()
+        {
+            double sumSquares = 0;
+            for (int i = 0; i < GetSize(); i++)
+            {
+                sumSquares += Math.Pow(Components[i], 2);
+            }
+            return Math.Sqrt(sumSquares);
+        }
+
+        public double GetComponent(int i)
+        {
+            if (i < 0 || i >= GetSize())
+            {
+                throw new ArgumentException("Компоненты с таким индексом нет");
+            }
+            return Components[i];
+        }
+
+        public void SetComponent(int i, double component)
+        {
+            if (i < 0 || i >= GetSize())
+            {
+                throw new ArgumentException("Компоненты с таким индексом нет");
+            }
+            Components[i] = component;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(obj, this))
+            {
+                return true;
+            }
+            if (ReferenceEquals(obj, null) || obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+            Vector p = (Vector)obj;
+            bool IsVectorsEquals = false;
+            if (GetSize() == p.GetSize())
+            {
+                IsVectorsEquals = true;
+                for (int i = 0; i < GetSize(); i++)
+                {
+                    if (Components[i] != p.Components[i])
+                    {
+                        IsVectorsEquals = false;
+                        break;
+                    }
+                }
+            }
+            return IsVectorsEquals;
+        }
+
+        public override int GetHashCode()
+        {
+            int prime = 13;
+            int hash = 1;
+            for (int i = 0; i < GetSize(); i++)
+            {
+                hash = prime * hash + Components[i].GetHashCode();
+            }
+            return hash;
+        }
+
+        public override string ToString()
+        {
+            return "{" + string.Join(" ; ", Components) + "}";
+        }
+
+        public static Vector GetSum(Vector firstVector, Vector secondVector)
+        {
+            int maxDimension = firstVector.GetSize();
+            if (maxDimension < secondVector.GetSize())
+            {
+                maxDimension = secondVector.GetSize();
+            }
+            Vector copySecondVector = new Vector(maxDimension, secondVector.Components);
+            Vector copyFirstVector = new Vector(maxDimension, firstVector.Components);
+            Vector sumVector = new Vector(maxDimension);
+            for (int i = 0; i < maxDimension; i++)
+            {
+                sumVector.Components[i] = copyFirstVector.Components[i] + copySecondVector.Components[i];
+            }
+            return sumVector;
+        }
+
+        public static Vector GetResidual(Vector firstVector, Vector secondVector)
+        {
+            int maxDimension = firstVector.GetSize();
+            if (maxDimension < secondVector.GetSize())
+            {
+                maxDimension = secondVector.GetSize();
+            }
+            Vector copySecondVector = new Vector(maxDimension, secondVector.Components);
+            Vector copyFirstVector = new Vector(maxDimension, firstVector.Components);
+            Vector sumVector = new Vector(maxDimension);
+            for (int i = 0; i < maxDimension; i++)
+            {
+                sumVector.Components[i] = copyFirstVector.Components[i] - copySecondVector.Components[i];
+            }
+            return sumVector;
+        }
+
+        public static Vector GetScalarMultiplication(Vector firstVector, double scalar)
+        {
+            Vector resultVector = new Vector(firstVector.GetSize());
+            for (int i = 0; i < firstVector.GetSize(); i++)
+            {
+                resultVector.Components[i] = firstVector.Components[i] * scalar;
+            }
+            return resultVector;
+        }
+    }
+}
